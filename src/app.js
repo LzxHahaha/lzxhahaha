da3b1f7e78a4e623a18481fc0fbd8677714280a5
LzxHahaha <lzxglhf@live.com>
2016-06-01 08:43:10 +0800

    添加跨域处理

--- src/app.js
+++ src/app.js
@@ -22,6 +22,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // 暂时设为*
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
+    next();
+  }
+});
+
 app.use('/', routes);
 app.use('/users', users);
 app.use('/post', post);
